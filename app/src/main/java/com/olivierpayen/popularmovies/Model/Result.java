a4fb3923a412afd18a93c7675996ee482dedb90d
Olivier Payen <olivier.payen@truecaller.com>
2015-12-21 16:29:42 +0100

    Add MovieList Fragment

---
+++ app/src/main/java/com/olivierpayen/popularmovies/Model/Result.java
@@ -0,0 +1,7 @@
+package com.olivierpayen.popularmovies.Model;
+
+/**
+ * Created by olivi on 12/21/2015.
+ */
+public class Result {
+}
---
+++ app/src/main/java/com/olivierpayen/popularmovies/MovieListFragment.java
@@ -0,0 +1,20 @@
+package com.olivierpayen.popularmovies;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MovieListFragment extends Fragment {
+
+    public MovieListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_movie_list, container, false);
+    }
+}
